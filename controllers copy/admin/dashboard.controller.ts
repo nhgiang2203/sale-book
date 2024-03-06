6b26615d82bd106f476f74f17fb3f7e76a89243e
Yang <nhgiang2203@gmail.com>
2024-03-07 02:59:06 +0700

    Del

--- controllers copy/admin/dashboard.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export const dashboard = (req: Request, res: Response) => {
-  res.render('admin/pages/dashboard/index', {
-    pageTitle: "Trang tổng quan"
-  })
-}
