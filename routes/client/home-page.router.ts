eea7f9be65f1b6ec8d27f06d6b1c862d941e91b3
Yang <nhgiang2203@gmail.com>
2024-03-07 22:36:02 +0700

    add

--- routes/client/home-page.router.ts
+++ routes/client/home-page.router.ts
@@ -3,6 +3,6 @@ const router: Router = Router();
 
 import * as controller from "../../controllers/client/home-page.controller";
 
-router.get("", controller.index);
+router.get("/", controller.index);
 
 export const homePageRoute = router;
